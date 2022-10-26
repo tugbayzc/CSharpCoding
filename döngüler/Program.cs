0b4d0d333a6233d56eba08e9d15647dba6fb74be
tugbayazici <yazicittugba@gmail.com>
2022-10-26 11:32:53 +0300

    for loop codes

---
+++ döngüler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace donguler 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(a+"x"+i+"="+a*i);
+
+            }
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if(n<=100)
+                   Console.WriteLine(new String(' ', n - i) + new String('#', i));
+            }
+        }
+    }
+}
